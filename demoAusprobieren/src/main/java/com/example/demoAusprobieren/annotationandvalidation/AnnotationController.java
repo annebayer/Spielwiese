65043d90c92584b4bd040f314441078429f1ec29
Anne Bayer <anne.bayer@andrena.de>
2025-01-09 13:36:30 +0100

    Refactore AnnotationController

--- demoAusprobieren/src/main/java/com/example/demoAusprobieren/annotationandvalidation/AnnotationController.java
+++ demoAusprobieren/src/main/java/com/example/demoAusprobieren/annotationandvalidation/AnnotationController.java
@@ -2,41 +2,43 @@ package com.example.demoausprobieren.annotationandvalidation;
 
 import java.util.Set;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AnnotationController {
-    @GetMapping("/annotation")
-    public String ersteSahne(@RequestParam(value = "notNegative", defaultValue = "12") int alter) {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Passt oder passt nicht? \n");
+    private static final String STANDARD_TEXT = "Passt oder passt nicht? \n";
+    private final Validator validator;
+
+
+    public AnnotationController() {
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        Validator validator = factory.getValidator();
-
-        KlasseMitNotNegativeAnnotation myBean = new KlasseMitNotNegativeAnnotation(alter);
-
-        Set<ConstraintViolation<KlasseMitNotNegativeAnnotation>> violations = validator.validate(myBean);
-
-        return buildString(stringBuilder, violations, myBean);
+        validator = factory.getValidator();
     }
 
-    private static String buildString(StringBuilder stringBuilder,
-            Set<ConstraintViolation<KlasseMitNotNegativeAnnotation>> violations,
+    @GetMapping("/annotation")
+    public String validateNumberNotNegative(@RequestParam(value = "notNegative", defaultValue = "12") int alter) {
+        KlasseMitNotNegativeAnnotation myBean = new KlasseMitNotNegativeAnnotation(alter);
+        Set<ConstraintViolation<KlasseMitNotNegativeAnnotation>> violations = validator.validate(myBean);
+
+        return generateResponseMessage(violations, myBean);
+    }
+
+    private static String generateResponseMessage(Set<ConstraintViolation<KlasseMitNotNegativeAnnotation>> violations,
             KlasseMitNotNegativeAnnotation myBean) {
-        if (!violations.isEmpty()) {
-            stringBuilder.append("Passt nicht!\nFolgende Verletzungen: \n");
-            for (ConstraintViolation<KlasseMitNotNegativeAnnotation> violation : violations) {
-                stringBuilder.append(violation.getMessage()).append("\n");
-            }
-        } else {
-            stringBuilder.append(String.format("Passt es oder nicht %s", myBean.getMyNotNegativeNumber()));
+        if (violations.isEmpty()) {
+            return String.format("%s Passt es oder nicht %s", STANDARD_TEXT, myBean.getMyNotNegativeNumber());
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(STANDARD_TEXT);
+        for (ConstraintViolation<KlasseMitNotNegativeAnnotation> violation : violations) {
+            stringBuilder.append(violation.getMessage()).append("\n");
         }
         return stringBuilder.toString();
     }
